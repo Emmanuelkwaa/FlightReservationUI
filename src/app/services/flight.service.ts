f5eff3c0815dacfa7c1e52395c8845b81457097f
JoeyPearson822 <Pearsonjoey@rocketmail.com>
2022-07-29 16:03:19 -0400

    flight service updated

--- src/app/services/flight.service.ts
+++ src/app/services/flight.service.ts
@@ -1,13 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Flight } from '../models/Flight';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightService {
+
+  flightToAdd: Flight = new Flight();
+
   flightUrl = "/flights"
 
   constructor(private http :HttpClient) { }
 
-   getAllFlight
+   getAllFlights (): Observable<Flight[]>{
+    return this.http.get<Flight[]>(this.flightUrl);
+    
+  }
+
+  addFlight(flight: Flight){
+    return this.http.post<Flight>(this.flightUrl, flight);
+    
+  }
+
+  update(flight: Flight){
+    return this.http.put<Flight>(this.flightUrl, Flight);
+    
+  }
+
+  // TO DO
+  // Probably needs to be changed
+  delete(id: number): any{
+    return this.http.delete<any>(this.flightUrl, {"body":{"id": id}});
+    
+  }
 }
