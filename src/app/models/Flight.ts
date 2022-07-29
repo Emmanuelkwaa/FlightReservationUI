b50133c9b116be9d4e06b6ac22c1d3e6d807166e
ONE BEAST <ONE BEAST@ONE-BEAST>
2022-07-29 14:21:45 -0400

    added flight model

---
+++ src/app/models/Flight.ts
@@ -0,0 +1,35 @@
+export class Flight {
+    
+    id? :number;
+    flightNumber :string;
+    departCity :City;
+    arrivalCity :City;
+    departDateTime :Date;
+    arrivalDateTime :Date;
+    economyPrice :number;
+    businessPrice :number;
+    airplane :Airplane;
+
+    constructor(
+        id = 0, 
+        flightNumber = '', 
+        departCity = new City(), 
+        arrivalCity = new departCity(),
+        departDateTime = new Date(),
+        arrivalDateTime = new Date(),
+        economyPrice = 0,
+        businessPrice = 0,
+        airplane = new Airplane
+    ) {
+        this.id = id;
+        this.flightNumber = flightNumber;
+        this.departCity = departCity;
+        this.arrivalCity = arrivalCity;
+        this.departDateTime = departDateTime;
+        this.arrivalDateTime = arrivalDateTime;
+        this.economyPrice = economyPrice;
+        this.businessPrice = businessPrice;
+        this.airplane = airplane;
+    }
+    
+}
